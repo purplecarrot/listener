072fa6018402922cfe029d8cac2d74d3ca7a909a
Mark <mark@purplecarrot.co.uk>
2021-12-23 16:05:40 +0000

    Don't process zero length strings

--- utils/env.go
+++ utils/env.go
@@ -12,12 +12,13 @@ func ProcessPortsEnv(port_strings []string) []int {
 	for _, port_string := range port_strings {
 
 		for _, chunk := range strings.Split(port_string, ",") {
-
-			port_int, err := strconv.Atoi(chunk)
-			if err != nil {
-				log.Printf("discarding '%s' as it is not a valid port number\n", chunk)
-			} else {
-				listen_ports = append(listen_ports, port_int)
+			if len(chunk) > 0 {
+				port_int, err := strconv.Atoi(chunk)
+				if err != nil {
+					log.Printf("discarding '%s' as it is not a valid port number\n", chunk)
+				} else {
+					listen_ports = append(listen_ports, port_int)
+				}
 			}
 		}
 	}
